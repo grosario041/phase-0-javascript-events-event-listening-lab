ab636a2b625828f739400f01882e0eca8ad2b1c4
Rosario Garcia <rosariogf041@gmail.com>
2023-01-21 23:01:15 -0500

    commit tree 556262bef27d1623dc5e409db767bc6e5209540d

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 function addingEventListener() {
+    for( var i = 0; i < items.length; i++ )
+        (function(i){
+            items[i].addEventListener('click', function(event) {
+                alert( i );
+            }, false);
+        })(i);
 }
